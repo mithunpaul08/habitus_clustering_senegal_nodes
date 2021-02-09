83dc8605e3fc77e22cd7e3b308323d12405458e9
mithunpaul08 <mithunpaul08@gmail.com>
2021-02-08 20:16:02 -0700

    adding a basic rough work which takes some random values, plots dendograms and also does agglomerative clustering

---
+++ atelier.py
@@ -0,0 +1,29 @@
+import sklearn
+from sklearn.cluster import AgglomerativeClustering
+import scipy.cluster.hierarchy as sch
+import numpy as np
+from numpy import random
+import pandas as pd
+import matplotlib.pyplot as plt
+import sys
+
+#dataset=pd.read_csv('./temp_data.csv')
+#X= dataset.iloc[:, [3, 4]].values
+
+X=random.rand(999,2)
+
+model=AgglomerativeClustering(n_clusters=None, distance_threshold=0.3, linkage='average',compute_full_tree=True)
+model.fit(X)
+labels=model.labels_
+
+plt.scatter(X[labels==0, 0], X[labels==0, 1], s=50, marker='o', color='red')
+plt.scatter(X[labels==1, 0], X[labels==1, 1], s=50, marker='o', color='blue')
+plt.scatter(X[labels==2, 0], X[labels==2, 1], s=50, marker='o', color='green')
+plt.scatter(X[labels==3, 0], X[labels==3, 1], s=50, marker='o', color='purple')
+plt.scatter(X[labels==4, 0], X[labels==4, 1], s=50, marker='o', color='orange')
+plt.show()
+
+
+# plt.figure(figsize=(15, 12))
+# dendo=sch.dendrogram(sch.linkage(X,method='average'))
+# plt.show()
