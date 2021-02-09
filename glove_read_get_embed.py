822163e262f5cf3655bff650337ce4e0553ddf7f
mithunpaul08 <mithunpaul08@gmail.com>
2021-02-09 00:45:01 -0700

    got all embeddings of causes alone as an array. now need to get the clustering done. but now getting some array shape error.. i have done this before..hold on

--- glove_read_get_embed.py
+++ glove_read_get_embed.py
@@ -1,20 +1,43 @@
 import sys
 from sklearn.metrics.pairwise import cosine_similarity
 import numpy as np
+from numpy import random
+import pandas as pd
 
 words_embeddings={}
-with open('glove_bottom10.txt',encoding='utf-8',mode='r') as f:
-    for line in f:
-        all_words=line.split()
-        word=all_words[0]
-        emb=np.array(all_words[1:len(all_words)])
-        embdash=np.reshape(emb, [1, -1])
-        words_embeddings[word]=embdash
+def read_from_glove_assimilate():
+    df = pd.read_csv('./glove_bottom10.txt')
+    for index,line in df.iterrows():
+            all_words=line[0].split()
+            word=all_words[0]
+            emb=np.array(all_words[1:len(all_words)])
+            emb=emb.astype(np.float)
+            embdash=np.reshape(emb, [1, -1])
+            words_embeddings[word]=embdash
+
+read_from_glove_assimilate()
+def get_embedding_given_token(tk):
+    assert words_embeddings is not None
+    assert len(words_embeddings.keys()) > 0
+    emb_word = words_embeddings.get(tk,None)
+    flag_out_of_vocab=False
+    #if the word is out of glove vocab, create a random initialization
+    #todo: switch to xavier glorot instead of random.rand
+    if emb_word is None:
+        flag_out_of_vocab= True
+        emb_word=random.rand(1,300)
+    assert emb_word is not None
+    return emb_word, flag_out_of_vocab
 
 
 
+def return_pairwise(word1,word2):
+    read_from_glove_assimilate()
+    emb_word1=get_embedding_given_token(word1)
+    emb_word2 = get_embedding_given_token(word2)
+    return cosine_similarity(emb_word1, emb_word2)
 
 
-
-pairwise_similarities=cosine_similarity(words_embeddings['xalisae'], words_embeddings['wried'])
-print(pairwise_similarities)
+#
+# cos=return_pairwise('zulchzulu','wried')
+# print(cos)
--- hac_scikit_atelier.py
+++ hac_scikit_atelier.py
@@ -6,12 +6,47 @@ from numpy import random
 import pandas as pd
 import matplotlib.pyplot as plt
 import sys
+from glove_read_get_embed import get_embedding_given_token
 
-#dataset=pd.read_csv('./temp_data.csv')
-#X= dataset.iloc[:, [3, 4]].values
+#read causes and effects
+dataset=pd.read_csv('./senegal_africa.csv')
+causes = dataset.iloc[:, [2]].values
 
-X=random.rand(999,2)
+# effects = dataset.iloc[:, [5]].values
+# assert causes.shape == effects.shape
 
+#for each token in cause and effect, get their glove embedding
+#Average embeddings for multi-word concepts
+
+
+total_oov_words=0
+total_tokens=0
+concept_emb={}
+X=[]
+for cause in causes:
+    concept_name=cause[0]
+    cause_split_tokens=concept_name.split()
+    total=[]
+    z = np.zeros(300)
+    emb_total= z.reshape(1,-1)
+
+    #for each sub token, get embedding of them, and get the average of all n token embeddings as the concepts overall embedding value
+    for each_token in cause_split_tokens:
+        total_tokens+=1
+        emb,flag_oov=get_embedding_given_token(each_token)
+        emb_total+=emb
+        if (flag_oov==True):
+            total_oov_words+=1
+    avg_emb=np.average(emb_total)
+    concept_emb[concept_name]=avg_emb
+    X.append(avg_emb)
+
+
+
+print(f"total number of oov tokens/total tokens={total_oov_words/total_tokens}")
+
+X=np.asarray(X).reshape(1,-1)
+#the engine part which does clustering and plotting. will need cosine similarities of each concept as input
 model=AgglomerativeClustering(n_clusters=None, distance_threshold=0.3, linkage='average',compute_full_tree=True)
 model.fit(X)
 labels=model.labels_
