cc0cb2cd4e6b8f3d1335dc518544e53cfdd6f83f
mithunpaul08 <mithunpaul08@gmail.com>
2021-02-08 22:52:25 -0700

    gets pairwise similarity of two word embeddings in glove. next load words from hoangs csv..i guess glove we will have to load on clara itself

--- glove_read_get_embed.py
+++ glove_read_get_embed.py
@@ -1,14 +1,19 @@
 import sys
 from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
 
 words_embeddings={}
 with open('glove_bottom10.txt',encoding='utf-8',mode='r') as f:
     for line in f:
         all_words=line.split()
         word=all_words[0]
-        print(word)
-        emb=all_words[1:len(all_words)]
-        words_embeddings[word]=emb
+        emb=np.array(all_words[1:len(all_words)])
+        embdash=np.reshape(emb, [1, -1])
+        words_embeddings[word]=embdash
+
+
+
+
 
 
 pairwise_similarities=cosine_similarity(words_embeddings['xalisae'], words_embeddings['wried'])
