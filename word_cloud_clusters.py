72e84f96b2cf78e5faf47bb0d87aa5a0008c5f07
mithunpaul08 <mithunpaul08@gmail.com>
2021-02-16 20:53:46 -0700

    able to put one wordcloud inside an oval shape. next try plotting multiple clouds

--- word_cloud_clusters.py
+++ word_cloud_clusters.py
@@ -8,16 +8,29 @@ from os import path
 from PIL import Image
 from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
 
+def transform_format(val):
+    if val == 0:
+        return 255
+    else:
+        return val
+
 dict_concepts={}
 dict_concepts["rice "]=1
 dict_concepts["rice hullers"]=1
 dict_concepts["rice productivity"]=100
 dict_concepts["more rice"]=1
 
-oval_mask = np.array(Image.open("img/oval.png"))
+oval_mask = np.array(Image.open("img/phploeBuh.png"))
+# transformed_wine_mask = np.ndarray((oval_mask.shape[0],oval_mask.shape[1]), np.int32)
+# for i in range(len(oval_mask)):
+#     transformed_wine_mask[i] = list(map(transform_format, oval_mask[i]))
+
+
 # Create and generate a word cloud image:
-wordcloud = WordCloud(prefer_horizontal=1,width=400,height=400,
+wordcloud = WordCloud(prefer_horizontal=1,width=1025,height=1000,
+                      mask=oval_mask,
                       background_color='white',
+contour_width=5, contour_color='black',
                         #max_font_size=500,
                       #min_font_size=4
                       ).generate_from_frequencies(dict_concepts)
