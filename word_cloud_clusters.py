b9f6a7d81d54e34d8e8540d72889090f7e35f8b3
mithunpaul08 <mithunpaul08@gmail.com>
2021-02-16 21:30:20 -0700

    able to plot 4 sub plots of word clusters in onee figure. next bring in data

--- word_cloud_clusters.py
+++ word_cloud_clusters.py
@@ -7,23 +7,57 @@ import pandas as pd
 from os import path
 from PIL import Image
 from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
+import sys
+from numpy import random
 
-def transform_format(val):
-    if val == 0:
-        return 255
-    else:
-        return val
+def display_wordcloud(dict_concepts, title, n_components):
+    #plt.figure()
+    plt.figure(figsize=(40, 10), facecolor='black')
+    j = np.ceil(n_components/4)
+    for t in range(n_components):
+        i=t+1
+        plt.subplot(j, 4, i)
+            #.set_title("Topic #" + str(t))
+        plt.plot()
+        oval_mask = np.array(Image.open("img/phploeBuh.png"))
+
+        wordcloud = WordCloud(prefer_horizontal=1, width=4444, height=4444,
+                              mask=oval_mask,
+                              background_color='white',
+                              contour_width=5, contour_color='black',
+                              # max_font_size=500,
+                              # min_font_size=4
+                              ).generate_from_frequencies(dict_concepts)
+
+        # Display the generated image:
+
+        plt.imshow(wordcloud, interpolation='bilinear')
+        # plt.axis("off")
+        # plt.show()
+
+        #plt.imshow(WordCloud().fit_words(top_words[t]))
+        plt.axis("off")
+    #fig.suptitle(title)
+    plt.show()
+
 
 dict_concepts={}
 dict_concepts["rice "]=1
 dict_concepts["rice hullers"]=1
-dict_concepts["rice productivity"]=100
+dict_concepts["rice productivity"]=1330
 dict_concepts["more rice"]=1
 
+
+top_words={}
+top_words[0]=["rice","rice","rice","rice","rice","rice",]
+top_words[1]=["corn","corn","corn","corn"]
+top_words[2]=["cnn","abc","nbs","fox"]
+top_words[3]=["donkey","monkey","giraffe","dog","cat","lizard",]
+
+display_wordcloud(dict_concepts,"name of plot", 4)
+sys.exit()
+
 oval_mask = np.array(Image.open("img/phploeBuh.png"))
-# transformed_wine_mask = np.ndarray((oval_mask.shape[0],oval_mask.shape[1]), np.int32)
-# for i in range(len(oval_mask)):
-#     transformed_wine_mask[i] = list(map(transform_format, oval_mask[i]))
 
 
 # Create and generate a word cloud image:
