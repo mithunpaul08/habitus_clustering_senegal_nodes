e0b7439a50b0b01aef4527859a616b83e3deb2cb
mithunpaul08 <mithunpaul08@gmail.com>
2021-02-19 20:12:42 -0700

    wrote code to get the probability given a token..however, its not getting probability...need to get it from logits again

--- direction_validation.py
+++ direction_validation.py
@@ -3,8 +3,39 @@ import torch
 from glove_read_get_embed import read_eidos_stopwords
 import sys
 import nltk
-nltk.download('stopwords')
+import torch.nn.functional as F
 from nltk.corpus import stopwords
+from transformers import pipeline
+from pprint import pprint
+
+
+
+
+import torch
+tokenizer = AutoTokenizer.from_pretrained("distilbert-base-cased")
+model = AutoModelForMaskedLM.from_pretrained("distilbert-base-cased")
+sequence = f"rainfall increases {tokenizer.mask_token}."
+input = tokenizer.encode(sequence, return_tensors="pt")
+mask_token_index = torch.where(input == tokenizer.mask_token_id)[1]
+token_logits = model(input).logits
+mask_token_logits = token_logits[0, mask_token_index, :]
+
+mask_token_logits=F.softmax(mask_token_logits)
+all_token_probs = torch.topk(mask_token_logits, len(tokenizer.get_vocab()), dim=1)
+alltoken_indices=all_token_probs[1]
+alltoken_probs=all_token_probs[0]
+token_probs={}
+for token_index,prob in zip(alltoken_indices[0],all_token_probs[0][0]):
+    token=tokenizer.decode(token_index)
+    token_probs[token]=prob
+
+print(token_probs['rice'])
+
+top_5_tokens = torch.topk(mask_token_logits, 5, dim=1).indices[0].tolist()
+for token in top_5_tokens:
+    print(sequence.replace(tokenizer.mask_token, tokenizer.decode([token])))
+
+sys.exit()
 
 eidos_stop_words = read_eidos_stopwords()
 tokenizer = AutoTokenizer.from_pretrained('bert-base-uncased')
@@ -16,9 +47,9 @@ mask_token_index=torch.where(input==tokenizer.mask_token_id)[1]
 token_logits=model(input).logits
 
 mask_token_logits = (token_logits[0, mask_token_index, :])
+mask_softmax=F.softmax(mask_token_logits)
 
-
-top_5_tokens = torch.topk(mask_token_logits, 5, dim=1).indices[0].tolist()
+top_5_tokens = torch.topk(mask_softmax, 5, dim=1).indices[0].tolist()
 for token in top_5_tokens:
     print(tokenizer.decode([token]))
 
