b0a32ec4a865b3f3e7b7afff721c0fb6c6434cc4
Mithun Paul <mithunpaul08@gmail.com>
2021-03-25 17:56:35 -0700

    combined marccos code with my MLM probability code. numbers match that ofmy previous results. atleast for education level to income for promotes verbs. next confirm code runs for income standard

--- calc_relation_probabilities.py
+++ calc_relation_probabilities.py
@@ -1,6 +1,7 @@
 import torch
 from transformers import AutoModelForMaskedLM, AutoTokenizer
-import torch.functional as F
+import torch.nn.functional as F
+from data.verbs import *
 
 MLM_MODEL="distilbert-base-uncased"
 tokenizer = AutoTokenizer.from_pretrained(MLM_MODEL)
@@ -8,17 +9,16 @@ model = AutoModelForMaskedLM.from_pretrained(MLM_MODEL)
 
 # prob(effect | cause)
 def create_prob_dict(sequence):
-        input = self.tokenizer.encode(sequence, return_tensors="pt")
-        mask_token_index = torch.where(input == self.tokenizer.mask_token_id)[1]
-        token_logits = self.model(input).logits
+        input = tokenizer.encode(sequence, return_tensors="pt")
+        mask_token_index = torch.where(input == tokenizer.mask_token_id)[1]
+        token_logits = model(input).logits
         mask_token_logits = token_logits[0, mask_token_index, :]
-
         mask_token_logits = F.softmax(mask_token_logits)
-        all_token_probs = torch.topk(mask_token_logits, len(self.tokenizer.get_vocab()), dim=1)
+        all_token_probs = torch.topk(mask_token_logits, len(tokenizer.get_vocab()), dim=1)
         alltoken_indices = all_token_probs[1]
         token_probs = {}
         for token_index, prob in zip(alltoken_indices[0], all_token_probs[0][0]):
-            token = self.tokenizer.decode(token_index)
+            token = tokenizer.decode(token_index)
             token_probs[token] = prob
         return token_probs,sequence
 
@@ -27,7 +27,6 @@ def prob(text_with_mask, expected_word):
     prob= token_probs[expected_word]
     return prob
 
-
 def calc_rel_prob(cause, effect, triggers):
     probabilities = []
     effect_tokens = effect.split(' ')
@@ -35,10 +34,12 @@ def calc_rel_prob(cause, effect, triggers):
         for i in range(len(effect_tokens)):
             effect_chunk = ' '.join(effect_tokens[:i])
             text = f'{cause} {trigger} {effect_chunk}[MASK]'
-            print(text)
-            #probabilities.append(prob(text, effect_tokens[i]))
-    #avg_prob = sum(probabilities) / len(probabilities)
-    return 0
+            prob_effect=prob(text, effect_tokens[i])
+            probabilities.append(prob_effect)
+            print(f"{text}:{prob_effect}")
+    avg_prob = float(sum(probabilities)) / float(len(probabilities))
+    return avg_prob
+
 def read_data(filename):
     with open(filename) as f:
         for line in f:
@@ -46,7 +47,7 @@ def read_data(filename):
             cause_synonyms = lhs.split('|')
             effect_synonyms = rhs.split('|')
             yield cause_synonyms, effect_synonyms
-promotes_triggers = ['promotes']
+promotes_triggers = all_promote_verbs
 filename="data/query_directionality_variables.csv"
 
 def calc_average_probabilities():
@@ -59,7 +60,8 @@ def calc_average_probabilities():
                 p = calc_rel_prob(cause, effect, promotes_triggers)
                 probabilities.append(p)
         # calculate probability average
-        avg_prob = sum(probabilities) / len(probabilities)
+        avg_prob = float(sum(probabilities)) / float(len(probabilities))
+        print(avg_prob)
         # TODO save to file
 
 
