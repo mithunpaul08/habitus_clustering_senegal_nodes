6e29a1c8ed54eee6138bd3fa4f65858a81410f30
mithunpaul08 <mithunpaul08@gmail.com>
2021-04-12 16:17:49 -0700

    added code to read both pdf files and human description. next: bring in nn

--- rte_pathways/get_pathways_rte.py
+++ rte_pathways/get_pathways_rte.py
@@ -1,4 +1,12 @@
 from utils import *
 
 if __name__=="__main__":
-    read_pdf_file("data/habitus_rice_growing_senegal/00.pdf")
+    # data=get_data("data/habitus_rice_growing_senegal/")
+    data_human_desc = get_data("data/human_description.txt")
+    data_pdfs=get_data_pdf_files("data/temp/")
+
+
+    for premise in data_human_desc:
+        for hyp in data_pdfs:
+            print(premise)
+            print(hyp)
--- rte_pathways/utils.py
+++ rte_pathways/utils.py
@@ -1,8 +1,42 @@
 import PyPDF2
+from os import listdir
+from os.path import isfile, join
+from tqdm import tqdm
+import itertools
 
-def read_pdf_file(filename):
-    file_obj = open(filename, 'rb')
-    pdf_reader = PyPDF2.PdfFileReader(file_obj)
-    print(pdf_reader.numPages)
-    pageObj = pdf_reader.getPage(32)
-    print(len(pageObj.extractText()))
+
+def get_data_pdf_files(folder_path):
+        all_text = []
+        file_count=len(listdir(folder_path))
+        assert file_count>0
+        for file in tqdm(listdir(folder_path),total=file_count):
+            try:
+                file_path=join(folder_path,file)
+                if isfile(file_path):
+                    file_obj = open(file_path, 'rb')
+                    pdf_reader = PyPDF2.PdfFileReader(file_obj)
+                    page_count = pdf_reader.numPages
+                    for x in range(page_count):
+                        pageObj = pdf_reader.getPage(x)
+                        page_data=pageObj.extractText()
+                        page_data_split=page_data.split("\n")
+                        all_text_per_page = []
+                        for line in page_data_split:
+                            if len(line)>1:
+                                all_text_per_page.append(line.strip().lower())
+                        all_text_per_page_str=". ".join(all_text_per_page)
+                        all_text.append(all_text_per_page_str)
+            except Exception:
+                print(Exception)
+                continue
+        assert len(all_text) > 0
+        return all_text
+
+def get_data(filename):
+    return open(filename,"r",newline='\n')
+
+
+
+
+
+
